0f4a6715d1ea00d14a980e5f078cebaa13cd8859
Jonathan Heaney <jonathan.heaney@gmail.com>
2023-04-02 20:39:33 -0500

    Finish chapter 4 exercises

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+public class Course {
+
+    private String instructor;
+    private String courseName;
+    private ArrayList<Student> studentRoster;
+}
